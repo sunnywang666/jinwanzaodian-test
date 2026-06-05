c6706d91c3d286db9acd8795786ab91a651e2066
sunnywang666 <happywxw066@gmail.com>
2026-06-05 20:14:57 +0800

    Set up Vite and GitHub Pages deployment

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "../prototype_v7.jsx";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
